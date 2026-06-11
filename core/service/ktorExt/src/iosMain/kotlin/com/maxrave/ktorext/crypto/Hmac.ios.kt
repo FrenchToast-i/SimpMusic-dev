63f5eb205f13ffd47b0c78ff3e46e29590810061
FrenchToast-i <jsullivan9922@gmail.com>
2026-06-11 09:16:03 -0400

    Simplify byte array conversion in Hmac.ios.kt

--- core/service/ktorExt/src/iosMain/kotlin/com/maxrave/ktorext/crypto/Hmac.ios.kt
+++ core/service/ktorExt/src/iosMain/kotlin/com/maxrave/ktorext/crypto/Hmac.ios.kt
@@ -54,10 +54,9 @@ actual class Hmac actual constructor(algorithm: String, private val secretKey: S
                     )
                 }
             }
-            val bytes = ByteArray(digestLength) { i ->
-                val ubyte: UByte = output[i]
-                ubyte.toInt().toByte()
-            }
+        val bytes = ByteArray(digestLength) { i ->
+            output[i].toInt().toByte()   
+        }
             Base64.encode(bytes)
         }
     }
